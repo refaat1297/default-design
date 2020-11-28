6e1dd33cb2a48115c3a0ae0fc693dda60531146f
o2microsys <engfadymondy@gmail.com>
2020-11-28 14:09:23 +0200

    add fonts

---
+++ src/views/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="home">
+        <h1>This is an home page</h1>
+        <i class="fal fa-abacus"></i>
+        <i class="fal fa-align-slash"></i>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+    name: 'Home',
+    components: {}
+}
+</script>
